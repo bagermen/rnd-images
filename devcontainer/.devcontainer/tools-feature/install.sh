18b924ac0fb1998e642e352bb141ff6461e62c28
bagermen <bagermen@yandex.ru>
2025-03-18 15:01:23 +0300

    feat: :art: update devcontainer to new approach
    update devcontainer to new approach

--- devcontainer/.devcontainer/tools-feature/install.sh
+++ devcontainer/.devcontainer/tools-feature/install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 RND_TOOL_VERSION=${VERSION}
+USERNAME=${USER}
 
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
 
-dotnet tool install --global RND.Tools.CmdLine --add-source github --version $RND_TOOL_VERSION
+sudo -u $USERNAME dotnet tool install --global RND.Tools.CmdLine --add-source github --version $RND_TOOL_VERSION
