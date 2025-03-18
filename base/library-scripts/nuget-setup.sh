01e9929079deaf5aa6ae2f59f1e740ef6f77a585
bagermen <bagermen@yandex.ru>
2025-03-18 19:47:33 +0300

    fix: 🐛 another attempt to fix dotnet settings (#318)

--- base/library-scripts/nuget-setup.sh
+++ base/library-scripts/nuget-setup.sh
@@ -24,24 +24,11 @@ fi
 mkdir -p $userhome/.nuget/NuGet/config
 echo '<?xml version="1.0" encoding="utf-8"?><configuration></configuration>' > $userhome/.nuget/NuGet/config/github.config
 dotnet nuget add source \
-	--username "%GITHUB_USERNAME%" \
-	--password "%GITHUB_PASSWORD%" \
+	--username "$GITHUB_USERNAME" \
+	--password "$GITHUB_PASSWORD" \
 	--store-password-in-clear-text \
-	--name github "https://nuget.pkg.github.com/%GITHUB_NAMESPACE%/index.json" \
+	--name github "https://nuget.pkg.github.com/$GITHUB_NAMESPACE/index.json" \
 	--configfile $userhome/.nuget/NuGet/config/github.config
-
-cat >> $userhome/.profile << EOF
-# Add .NET Core SDK tools
-if [ -d "\$HOME/.dotnet/tools" ] ; then
-    PATH="\$HOME/.dotnet/tools:\$PATH"
-fi
-EOF
-
-cat > /etc/profile.d/github-env.sh << EOF
-export GITHUB_USERNAME="$GITHUB_USERNAME"
-export GITHUB_PASSWORD="$GITHUB_PASSWORD"
-export GITHUB_NAMESPACE="$GITHUB_NAMESPACE"
-EOF
 
 chown -R ${USERNAME}:${group_name} "${userhome}/.profile"
 chown -R ${USERNAME}:${group_name} "${userhome}/.nuget/NuGet/config/github.config"
